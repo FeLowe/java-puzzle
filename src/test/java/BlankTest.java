f67af97c15055b0659327f57575fb711e501faa7
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2016-04-22 09:07:58 -0700

    create inicial folders and files as well as README file and project specs

--- src/test/java/BlankTest.java
+++ src/test/java/BlankTest.java
@@ -6,47 +6,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class BlankTest extends FluentTest{ // needs to change "BlankTest" according of how you named it on your BlankTest.java file//
-  public WebDriver webDriver = new HtmlUnitDriver();
+// public class PuzzleTest extends FluentTest{ // needs to change "BlankTest" according of how you named it on your BlankTest.java file//
+//   public WebDriver webDriver = new HtmlUnitDriver();
+//
+//   @Override
+//  public WebDriver getDefaultDriver() {
+//    return webDriver;
+//  }
+//
+//  @ClassRule
+//  public static ServerRule server = new ServerRule();
 
-  @Override
- public WebDriver getDefaultDriver() {
-   return webDriver;
+ // @Test
+ // public void rootTest() {
+ //   goTo("http://localhost:4567/");
+ //    fill("#???").with("?");// (fill: method that calls from your "home.vtl" file)(with: fake user iNPUT YOU CREATED JUST TO DO A TEST).
+ //    submit(".btn");
+ //    assertThat(pageSource()).contains("what you want to be found related to your method");
  }
-
- @ClassRule
- public static ServerRule server = new ServerRule();
-
- @Test
- public void rootTest() {
-   goTo("http://localhost:4567/");
-    fill("#???").with("?");// (fill: method that calls from your "home.vtl" file)(with: fake user iNPUT YOU CREATED JUST TO DO A TEST).
-    submit(".btn");
-    assertThat(pageSource()).contains("what you want to be found related to your method");
- }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   @Test
   public void runBlank_CheckifPageIsBlank_true(){
     Blank ->MAIN CLASS<- myBlank = new Blank();  ->THIS CREATES A INSTANCE THE MAIN CLASS<-
